bbd0bd1f7b6be4297357c708fb7677c7f47a055b
HERYY <fadildattul@egmail.com>
2026-09-05 19:26:11 +0800

    Initial project setup

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "smart_finance";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Koneksi database gagal: " . $conn->connect_error);
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Smart Financial Dashboard</title>
+</head>
+<body>
+
+    <h1>Smart Financial Dashboard</h1>
+
+    <p>Project berhasil dijalankan.</p>
+
+</body>
+</html>
